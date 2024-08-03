d6e2dffadb55c1a466e609926c87082f4bc804e9
Lasithruwantha <lasithruwanthalasith@gmail.com>
2024-08-03 19:25:29 +0530

    Add files via upload

---
+++ mathcal/No.py
@@ -0,0 +1,34 @@
+
+
+while(True):
+  i=0
+  print ("my math tools")
+  print ("[1] + ")
+  print ("[2] - ")
+  print ("[3] × ")
+  print ("[4] / ")
+  tool= int(input ("you tool >:"))
+  if (tool==int(1)):
+    number1=int(input ("Enteryou number:") )
+    number2=int(input ("Enter you number:") )
+    x= number1+number2
+    print (x)
+    print("this tool esi")
+  elif (tool==int(2)):
+    number3=int(input ("Enter you number:") )
+    number4=int(input ("Enter you number:") )
+    y=number3-number4
+    print (y)
+  elif (tool==int(3)):
+    number5=int(input ("Enter you number:") )
+    number6=int(input ("Enteryou nunmber:") )
+    z=number5*number6
+    print (z)
+  elif (tool==int(4):
+    number7=int(input ("Enter you number:") )
+    number8=int(input ("Enter you number:") )
+    a=number7/number8
+    print (a)
+  else:
+    print ("try agen")
+  print("tool by lasith")
